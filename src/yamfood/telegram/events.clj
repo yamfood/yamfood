ebe32445b586de2ebf907b491592e0549caec7ac
kensay98 <rustambabadjanov@mail.ru>
2019-12-11 22:48:58 +0200

    Fixed error in events

--- src/yamfood/telegram/events.clj
+++ src/yamfood/telegram/events.clj
@@ -1,9 +1,9 @@
 (ns yamfood.telegram.events
   (:require
-    [yamfood.telegram.handlers.text]
     [yamfood.telegram.handlers.start]
     [yamfood.telegram.handlers.order]
     [yamfood.telegram.handlers.reply]
     [yamfood.telegram.handlers.basket]
-    [yamfood.telegram.handlers.callback]
-    [yamfood.telegram.handlers.inline]))
+    [yamfood.telegram.handlers.inline]
+    [yamfood.telegram.handlers.product]
+    [yamfood.telegram.handlers.callback]))
