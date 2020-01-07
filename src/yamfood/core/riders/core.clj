c40dd4ae72c741417e451a6cdee9d5a436e81c92
kensay98 <rustambabadjanov@mail.ru>
2020-01-07 21:10:43 +0500

    Added rider auth

--- src/yamfood/core/riders/core.clj
+++ src/yamfood/core/riders/core.clj
@@ -1,1 +1,24 @@
-(ns yamfood.core.riders.core)
+(ns yamfood.core.riders.core
+  (:require
+    [honeysql.core :as hs]
+    [honeysql.helpers :as hh]
+    [clojure.java.jdbc :as jdbc]
+    [yamfood.core.db.core :as db]))
+
+
+(def rider-query
+  {:select   [:riders.id :riders.tid :riders.name :riders.phone]
+   :from     [:riders]
+   :order-by [:riders.id]})
+
+
+(defn rider-by-tid-query
+  [tid]
+  (hs/format (hh/merge-where rider-query [:= :riders.tid tid])))
+
+
+(defn rider-by-tid!
+  [tid]
+  (->> (rider-by-tid-query tid)
+       (jdbc/query db/db)
+       (first)))
--- src/yamfood/telegram/handlers/rider/core.clj
+++ src/yamfood/telegram/handlers/rider/core.clj
@@ -1,29 +1,32 @@
 (ns yamfood.telegram.handlers.rider.core
   (:require
     [environ.core :refer [env]]
-    [yamfood.telegram.dispatcher :as d]))
+    [yamfood.core.riders.core :as r]
+    [yamfood.telegram.dispatcher :as d]
+    [yamfood.telegram.handlers.utils :as u]))
 
 
 (defn build-ctx!
   [update]
   {:token  (env :rider-bot-token)
-   :update update})
+   :update update
+   :rider  (r/rider-by-tid! (u/tid-from-update update))})
 
 
 (defn process-message
   [ctx update]
   (let [message (:message update)
         text (:text message)]
-    (cond
-      text (d/dispatch! ctx [:r/text]))))
+    (d/dispatch! ctx [:r/text])))
 
 
 (defn rider-update-handler!
   [update]
   (let [message (:message update)
         ctx (build-ctx! update)]
-    (if message
-      (process-message ctx update))))
+    (when (:rider ctx)
+      (if message
+        (process-message ctx update)))))
 
 
 
