56b3c04562164d23be0304c24b184d049ac473a4
kensay98 <rustambabadjanov@mail.ru>
2020-01-23 17:20:31 +0500

    Added app_name env to app.json and modified webhooks script

--- setup_webhooks.sh
+++ setup_webhooks.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-webhook_url=$(heroku info -s | grep web_url | cut -d= -f2)updates
+webhook_url=$APP_NAME.herokuapp.com/updates
+
+echo $webhook_url
+
 curl "https://api.telegram.org/bot$BOT_TOKEN/setWebhook?url=$webhook_url/client"
 curl "https://api.telegram.org/bot$RIDER_BOT_TOKEN/setWebhook?url=$webhook_url/rider"
