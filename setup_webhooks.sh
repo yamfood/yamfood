4d2241478c64b8a16136b1ae22dc3ccc8f7b9bbc
kensay98 <rustambabadjanov@mail.ru>
2020-01-24 17:41:47 +0500

    Refactored setup_webhooks.sh

--- setup_webhooks.sh
+++ setup_webhooks.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-webhook_url=$APP_NAME.herokuapp.com/updates
-
-echo $webhook_url
+webhook_url=${APP_NAME}.herokuapp.com/updates
 
 curl "https://api.telegram.org/bot$BOT_TOKEN/setWebhook?url=$webhook_url/client"
 curl "https://api.telegram.org/bot$RIDER_BOT_TOKEN/setWebhook?url=$webhook_url/rider"
