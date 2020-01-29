c5b4a7427952294d705fbb83d5945ae94ff3ef65
kensay98 <rustambabadjanov@mail.ru>
2020-01-29 13:18:33 +0500

    Fixed bug with active order by adding rebuild ctx

--- src/yamfood/telegram/handlers/rider/order.clj
+++ src/yamfood/telegram/handlers/rider/order.clj
@@ -3,7 +3,8 @@
     [yamfood.core.orders.core :as o]
     [yamfood.core.riders.core :as r]
     [yamfood.telegram.dispatcher :as d]
-    [yamfood.telegram.handlers.utils :as u]))
+    [yamfood.telegram.handlers.utils :as u]
+    [yamfood.telegram.handlers.rider.core :as c]))
 
 
 (defn- order-detail-text
@@ -145,7 +146,9 @@
       :answer-callback {:callback_query_id (:id query)
                         :text              "Заказ отменен!"
                         :show_alert        true}
-      :dispatch        {:args [:r/menu]}
+      :dispatch        {:args        [:r/menu]
+                        :rebuild-ctx {:function c/build-ctx!
+                                      :update   update}}
       :delete-message  {:chat-id    chat-id
                         :message-id message-id}})))
 
