b81ca158cc9cec9e965d7627f6e147d2ba2403d1
kensay98 <rustambabadjanov@mail.ru>
2020-03-07 17:04:21 +0500

    Removed unique constrain in admin login

--- resources/migrations/admins/20200204120841-admins.up.sql
+++ resources/migrations/admins/20200204120841-admins.up.sql
@@ -1,7 +1,7 @@
 create table "admins"
 (
     id       serial       not null,
-    login    varchar(200) not null unique,
+    login    varchar(200) not null,
     password varchar(200) not null,
     payload  jsonb        not null default '{}',
     token    varchar(200) unique,
