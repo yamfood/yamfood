56604ea714f0c74d57c8f056d422367a68073da0
kensay98 <rustambabadjanov@mail.ru>
2020-04-05 18:15:45 +0500

    Added s3 integration module and API for getting signed urls

--- src/yamfood/api/admin/core.clj
+++ src/yamfood/api/admin/core.clj
@@ -1,6 +1,9 @@
 (ns yamfood.api.admin.core
   (:require
+    [yamfood.utils :as u]
     [compojure.core :as c]
+    [clojure.string :as str]
+    [yamfood.integrations.s3.core :as s3]
     [yamfood.api.admin.handlers.auth :as auth]
     [yamfood.api.admin.handlers.riders :as riders]
     [yamfood.api.admin.handlers.admins :as admins]
@@ -12,11 +15,27 @@
     [yamfood.api.admin.handlers.announcements :as announcements]))
 
 
+(defn sign-s3
+  [request]
+  (let [params (:params request)
+        folder (get params "folder" "default")
+        file-name (get params "file-name")
+        file-type (second (str/split file-name #"\."))
+        file-name (str (u/uuid) "." file-type)
+        key (str folder "/" file-name)]
+    {}
+    {:body {:url           (format "https://s3-eu-west-1.amazonaws.com/%s/%s"
+                                   s3/bucket
+                                   key)
+            :signedRequest (str (s3/generate-presigned-url! key))}}))
+
+
 (c/defroutes
   routes
   (c/context "/auth" [] auth/routes)
   (wrap-auth
     (c/routes
+      (c/GET "/sign-s3" [] sign-s3)
       (c/context "/admins" [] admins/routes)
       (c/context "/riders" [] riders/routes)
       (c/context "/orders" [] orders/routes)
