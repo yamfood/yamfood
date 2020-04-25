d56540e8c3b6463c8b0eea2c16d541031ab147b4
kensay98 <rustambabadjanov@mail.ru>
2020-04-25 19:28:33 +0500

    Added uzbek translation

--- src/yamfood/telegram/translation/core.clj
+++ src/yamfood/telegram/translation/core.clj
@@ -32,7 +32,7 @@
         ; Update location
         :update-location-inline-button   "Коснитесь чтобы обновить\n"
         :send-current-location-button    "Отправить текущее положение"
-        :new-location-message            "Новый адресс: {1}"
+        :new-location-message            "Новый адрес: {1}"
         :request-location-message        (str "*куда доставить?*\n\n"
                                               "нажмите «Отправить текущее местоположение» или отправьте локацию вручную\n\n"
                                               "_не забудьте включить локацию на своем телефоне_")
@@ -86,7 +86,7 @@
         :status-on-way                   "Курьер уже в пути!"
 
         ; Feedback
-        :request-feedback-message        "Оцените пожалуйста заказ!"
+        :request-feedback-message        "Оцените, пожалуйста, заказ!"
 
         ; Other
         :unhandled-text                  "Не понял"
@@ -122,9 +122,9 @@
         :update-location-inline-button   "Tap to update\n"
         :send-current-location-button    "Send current location"
         :new-location-message            "New address: {1}"
-        :request-location-message        (str "*where to deliver?*\n\n"
+        :request-location-message        (str "*Where to deliver?*\n\n"
                                               "Tap «Send current location» or send the location manually\n\n"
-                                              "_don't forget to enable location on your phone..._")
+                                              "_don't forget to enable location on your phone_")
 
         ; Invalid-location
         :invalid-location-message        "Unfortunately, we do not serve this region"
@@ -181,7 +181,96 @@
         :blocked-message                 "You are blocked, please contact the support service"
         :accepted                        "Accepted"
         :card                            "By card"
-        :cash                            "In cash"}})
+        :cash                            "In cash"}
+   :uz {; Registration
+        :send-contact-button             "Telefon raqamini yuborish"
+        :change-phone-button             "Telefon raqamini o'zgartirish"
+        :request-code-message            "Telefon raqamingizga yuborilgan kodni yuboring _+{1}_"
+        :invalid-phone-message           "Noto'g'ri telefon raqami, yana urinib ko'ring"
+        :phone-confirmed-message         "Telefon raqami tasdiqlangan!"
+        :incorrect-code-message          "Noto'g'ri kod, yana urinib ko'ring"
+        :request-contact-message         (str "Kontaktingizni yoki telefon raqamingizni formatda yuboring _998901234567_\n\n"
+                                              "Tasdiqlash uchun kod bilan SMS yuboramiz")
+
+        ; Start
+        :hello-message                   "Nimadan boshlaymiz?"
+        :menu-button                     "\uD83C\uDF7D Nima ovqatlar bor?"
+        :regions-button                  (str e/location-emoji " Qoplama maydoni")
+        :settings-button                 (str e/settings-emoji " Sozlamalar")
+
+        ; Settings
+        :settings-change-phone-button    "Telefon raqamini o'zgartirish"
+        :settings-menu-button            (str e/back-emoji " Orqaga")
+        :settings-message                (str e/settings-emoji " *Sozlamalar* \n\n"
+                                              "*Til*: O'zbekcha\n"
+                                              "*Telefon raqami*: +{1}\n\n"
+                                              "_Tilni o'zgartirish uchun tegishli tugmani bosing_")
+
+        ; Update location
+        :update-location-inline-button   "Yangilash uchun bosing\n"
+        :send-current-location-button    "Joriy manzilni yuborish"
+        :new-location-message            "Yangi manzil: {1}"
+        :request-location-message        (str "*qayerga yetkazib berish kerak?*\n\n"
+                                              "«Joriy manzilni yuborish» tugmasini bosing yoki joyni qo'lda yuboring\n\n"
+                                              "_telefoningizdagi joyni yoqishni unutmang_")
+
+        ; Invalid-location
+        :invalid-location-message        "Afsuski, biz ushbu manzilda xizmat qilmaymiz"
+        :invalid-location-regions-button "Xizmat kartasi"
+        :invalid-location-menu-button    (str e/back-emoji " Menyu")
+        :invalid-location-basket-button  (str e/basket-emoji " Savat")
+
+        ; Product Details
+        :added-to-basket-message         "Savatga qo'shilgan"
+        :more-button                     "\uD83C\uDF7D Yana?"
+        :add-product-button              "Istayman"
+        :product-basket-button           (str e/basket-emoji " Savat ({1} so'm)")
+        :product-menu-button             (str e/back-emoji " Назад")
+        :product-caption                 (str e/food-emoji " *{name}* \n\n"
+                                              "{description}"
+                                              e/money-emoji "{price} so'm")
+
+        ; Basket
+        :basket-message                  "Sizning savatingiz:"
+        :empty-basket-text               "Afsuski, sizning savatingiz hali bo'sh :("
+        :basket-menu-button              (str e/back-emoji " Menyuga")
+        :to-order-button                 "✅ Keyingi"
+
+        ; Order Confirmation
+        :oc-basket-button                (str e/back-emoji " Savat")
+        :oc-create-order-button          "✅ Tasdiqlash"
+        :oc-empty-comment-text           "Bo'sh"
+        :oc-message                      (str "*Buyurtmaning tafsilotlari:* \n\n"
+                                              e/money-emoji " {1} so'm ({2})\n"
+                                              e/comment-emoji " `{3}` \n\n"
+                                              e/location-emoji " {4}")
+
+        ; Active Order
+        :active-order-message            (str "*Buyurtma №{1}:*\n\n"
+                                              "{2}"
+                                              "\n"
+                                              e/money-emoji " {3} so'm ({4})\n\n"
+                                              "Sizning buyurtmangiz qabul qilindi, tasdiqlashni kuting")
+
+        ; Payments
+        :pay-button                      "To'lash"
+        :invoice-cancel-button           "Orqaga"
+        :invoice-title                   "Buyurtmani to'lash №{1}"
+
+        ; Statuses
+        :status-on-kitchen               "Sizning buyurtmangiz tayyorlanyapti, kuryer 60 daqiqada yoki undan oldin keladi!"
+        :status-canceled                 "Buyurtma bekor qilindi :("
+        :status-on-way                   "Kuryer yo'lda!"
+
+        ; Feedback
+        :request-feedback-message        "Iltimos, buyurtmani baholang!"
+
+        ; Other
+        :unhandled-text                  "Tushunmadim"
+        :blocked-message                 "Siz bloklangindiz, qo'llab-quvvatlash xizmatiga murojaat qiling"
+        :accepted                        "Qabul qilindi"
+        :card                            "Karta bilan"
+        :cash                            "Naqd pul bilan"}})
 
 
 (def translate
