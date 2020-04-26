33ee30917a007a487cd35841ac4f198eebbec42c
kensay98 <rustambabadjanov@mail.ru>
2020-04-26 13:07:33 +0500

    Fixed migrations and added core functions for params

--- resources/migrations/params/20200426065117-params.up.sql
+++ resources/migrations/params/20200426065117-params.up.sql
@@ -2,6 +2,8 @@ create table params
 (
     id    serial,
     name  varchar(200) not null,
+    key   varchar(200) not null unique,
+    docs  text         not null default '',
     value varchar      not null,
 
     primary key (id)
--- src/yamfood/core/params/core.clj
+++ src/yamfood/core/params/core.clj
@@ -1,32 +1,55 @@
 (ns yamfood.core.params.core
   (:require
-    [clojure.edn :as edn]
     [honeysql.core :as hs]
+    [environ.core :refer [env]]
     [clojure.java.jdbc :as jdbc]
-    [yamfood.core.db.core :as db]))
+    [yamfood.core.db.core :as db]
+    [clojure.set :refer [difference]]))
 
 
 (def params-query
   {:select [:params.name
-            :params.value]
+            :params.key
+            :params.value
+            :params.docs]
    :from   [:params]})
+
+
+(def all-params
+  {:delivery-cost    {:name    "Сумма за доставку"
+                      :docs    ""
+                      :default "10000"
+                      :adapter read-string}
+   :iiko-enabled?    {:name    "Отправлять заказы в iiko?"
+                      :docs    ""
+                      :default "false"
+                      :adapter read-string}
+   :iiko-user-id     {:name    "IIKO User ID"
+                      :docs    ""
+                      :default (env :iiko-user-id)
+                      :adapter nil}
+   :iiko-user-secret {:name    "IIKO User Secret"
+                      :docs    ""
+                      :default (env :iiko-user-secret)
+                      :adapter nil}})
+
+
+(defn params-reducer
+  [r param]
+  (let [key (keyword (:key param))
+        adapter (:adapter (get all-params key))
+        value (if adapter
+                (adapter (:value param))
+                (:value param))]
+    (assoc r key value)))
 
 
 (defn params->map
   [params-list]
   (reduce
-    #(assoc
-       %1
-       (keyword (:name %2))
-       (edn/read-string (:value %2)))
+    params-reducer
     {}
     params-list))
-
-
-(def default-params
-  {:disable-card false
-   :verify-phone true
-   :delivery-cost 10000})
 
 
 (defn params!
@@ -34,5 +57,34 @@
   (->> params-query
        (hs/format)
        (jdbc/query db/db)
-       (params->map)
-       (merge default-params)))
+       (params->map)))
+
+
+(defn absent-params!
+  []
+  (let [existing (set (keys (params!)))
+        all (set (keys all-params))
+        to-create (vec (difference all existing))]
+    (select-keys all-params to-create)))
+
+
+(defn create-params!
+  [params]
+  (let [params (map (fn [param]
+                      (let [key (first param)
+                            p (second param)]
+                        {:key   (name key)
+                         :name  (:name p)
+                         :value (:default p)
+                         :docs  (:docs p)}))
+                    (seq params))]
+    (jdbc/insert-multi!
+      db/db
+      "params"
+      params)))
+
+
+(defn sync-params!
+  []
+  (-> (absent-params!)
+      (create-params!)))
