bf9fcd19df1354efbaf76653dee01378d28599a8
kensay98 <rustambabadjanov@mail.ru>
2020-04-27 17:04:59 +0500

    Added core functions for sms

--- src/yamfood/core/sms/core.clj
+++ src/yamfood/core/sms/core.clj
@@ -1,1 +1,49 @@
-(ns yamfood.core.sms.core)
+(ns yamfood.core.sms.core
+  (:require
+    [honeysql.core :as hs]
+    [honeysql.helpers :as hh]
+    [environ.core :refer [env]]
+    [clojure.java.jdbc :as jdbc]
+    [yamfood.core.db.core :as db]
+    [clojure.set :refer [difference]]))
+
+
+(def all-sms-query
+  {:select [:sms.id
+            :sms.text
+            :sms.phone
+            :sms.created_at
+            :sms.is_sent
+            :sms.error]
+   :from   [:sms]})
+
+
+(defn all-sms!
+  ([]
+   (all-sms! 0 100))
+  ([offset limit]
+   (->> (-> all-sms-query
+            (assoc :offset offset)
+            (assoc :limit limit)
+            (hs/format))
+        (jdbc/query db/db))))
+
+
+(defn sms-to-send!
+  [limit]
+  (->> (-> all-sms-query
+           (assoc :limit limit)
+           (hh/merge-where [:and
+                            [:< :sms.created_at :current_timestamp]
+                            [:= :sms.is_sent false]])
+           (hs/format))
+       (jdbc/query db/db)))
+
+
+(defn update!
+  [sms-id row]
+  (jdbc/update!
+    db/db
+    "sms"
+    row
+    ["id = ?" sms-id]))
---
+++ src/yamfood/tasks/sms.clj
@@ -0,0 +1,1 @@
+(ns yamfood.tasks.sms)
