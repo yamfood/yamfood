15779b1997b465fdb6afe2eb64c4c2eaa1067c41
kensay98 <rustambabadjanov@mail.ru>
2020-05-27 20:07:33 +0500

    Removed product_id uniqueness in basket_products and order_products

---
+++ resources/migrations/basket/20200527150210-basket-products-uniqueness.up.sql
@@ -0,0 +1,2 @@
+alter table basket_products
+    drop constraint basket_products_basket_id_product_id_key;
---
+++ resources/migrations/orders/20200527150452-order-products-uniqueness.up.sql
@@ -0,0 +1,2 @@
+alter table order_products
+    drop constraint order_products_order_id_product_id_key;
